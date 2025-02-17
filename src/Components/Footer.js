ebd9ccf8951fa62048c76fb1bc3c541fd91d5605
Tristan David Ian Collett <tristancollett8787@gmail.com>
2025-02-17 14:34:15 +0200

    Updated Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import "../Styles/Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; @2025 Tristan David Ian Collett. All rights reserved.</p>
+      <p>&copy; 2025 Tristan David Ian Collett. All rights reserved.</p>
     </footer>
   );
 }
